8ed936b2f129a193469ce4f03bf7efcb6e5d14fe
Stefano Borzì <stefanoborzi32@gmail.com>
2026-03-05 17:39:26 +0100

    fix: show stats (#54)

--- src/solo3v3_sc.cpp
+++ src/solo3v3_sc.cpp
@@ -379,18 +379,19 @@ bool NpcSolo3v3::OnGossipSelect(Player* player, Creature* creature, uint32 /*sen
                 ChatHandler(player->GetSession()).PSendSysMessage("{}", s.str().c_str());
                 CloseGossipMenuFor(player);
 
-                ArenaTeam::MemberList::iterator itr;
-                for (itr = at->GetMembers().begin(); itr != at->GetMembers().end(); ++itr)
-                {
-                    if (itr->Guid == player->GetGUID())
-                    {
-                        std::stringstream s;
-                        s << "\nSolo MMR: " << itr->MatchMakerRating;
+                // Query MMR from database
+                CharacterDatabasePreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_SEL_MATCH_MAKER_RATING);
+                stmt->SetData(0, player->GetGUID().GetCounter());
+                stmt->SetData(1, ARENA_SLOT_SOLO_3v3);
+                PreparedQueryResult result = CharacterDatabase.Query(stmt);
 
-                        ChatHandler(player->GetSession()).PSendSysMessage("{}", s.str().c_str());
-                        break;
-                    }
-                }
+                uint16 matchMakerRating;
+                if (result)
+                    matchMakerRating = (*result)[0].Get<uint16>();
+                else
+                    matchMakerRating = sWorld->getIntConfig(CONFIG_ARENA_START_MATCHMAKER_RATING);
+
+                ChatHandler(player->GetSession()).PSendSysMessage("Solo MMR: {}", matchMakerRating);
             }
 
             return true;
@@ -1011,7 +1012,7 @@ void PlayerScript3v3Arena::OnPlayerGetArenaPersonalRating(Player* player, uint8 
 {
     if (slot == ARENA_SLOT_SOLO_3v3)
     {
-        if (ArenaTeam* at = sArenaTeamMgr->GetArenaTeamByCaptain(player->GetGUID(), ARENA_TYPE_3v3_SOLO))
+        if (ArenaTeam* at = sArenaTeamMgr->GetArenaTeamById(player->GetArenaTeamId(ARENA_SLOT_SOLO_3v3)))
         {
             rating = at->GetRating();
         }
@@ -1027,7 +1028,7 @@ void PlayerScript3v3Arena::OnPlayerGetMaxPersonalArenaRatingRequirement(const Pl
 
     if (minslot < 6)
     {
-        if (ArenaTeam* at = sArenaTeamMgr->GetArenaTeamByCaptain(player->GetGUID(), ARENA_TYPE_3v3_SOLO))
+        if (ArenaTeam* at = sArenaTeamMgr->GetArenaTeamById(player->GetArenaTeamId(ARENA_SLOT_SOLO_3v3)))
         {
             maxArenaRating = std::max(at->GetRating(), maxArenaRating);
         }
